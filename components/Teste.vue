f89d45c6edabbd696c5bb79bfbd192141c852f17
Guilherme Constantinov <guilherme_constantinov@hotmail.com>
2022-09-04 20:39:00 -0300

    initial commit

---
+++ components/Teste.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>teste: {{ user }}</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class Teste extends Vue {
+  @Prop()
+  user!: string
+}
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Teste user="teste" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import Teste from '~/components/Teste.vue'
+
+@Component({ components: { Teste } })
+export default class PageIndex extends Vue {}
+</script>
